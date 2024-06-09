822bde1a85252e547c6f4c4e0704231c28f13345
GouravMidya <gouravgmidya@gmail.com>
2024-06-09 12:25:15 +0530

    integrated the RAG agent in the fastapi backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
+import csv
+from datetime import datetime
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from langchain.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.chat_models import ChatOllama
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 app = FastAPI()
 
@@ -19,24 +25,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load pre-trained GPT-2 model and tokenizer
-model_name = "gpt2-medium"  # You can use other sizes of GPT-2 as well
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-model = GPT2LMHeadModel.from_pretrained(model_name)
+# Initialize necessary components
+local_llm = 'phi3'  # llama3
+embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+persist_directory = "C:/Users/goura/Documents/humanaize fintech 24/chroma/"
+vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+retriever = vectordb.as_retriever(search_type="mmr")
+llm = ChatOllama(model=local_llm, format="json", temperature=0)
 
-# Set the model to evaluation mode
-model.eval()
+prompt = PromptTemplate(
+    template="""You are an assistant for question-answering tasks.
+    Use the following pieces of retrieved context to answer the question
+    meaningfully. If you don't know the answer, just say that you don't know.
+    Use six sentences maximum and keep the answer concise and respond in
+    json
+    {context}
+    Question: {question}
+    Helpful Answer:""",
+    input_variables=["question", "context"],
+)
+
+rag_chain = prompt | llm | StrOutputParser()
 
 class Query(BaseModel):
     query: str
 
 @app.post("/api/query")
 async def get_response(query: Query):
-    input_text = query.query
-    inputs = tokenizer.encode(input_text, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
-    response_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return {"response": response_text}
+    question = query.query
+    start_time = datetime.now()
+    docs = retriever.invoke(question)
+    generation = rag_chain.invoke({"context": docs, "question": question})
+    end_time = datetime.now()
+    time_taken = end_time - start_time
+
+    # Log the query, answer, and time taken in a CSV file
+    with open('logs/log.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([question, generation, time_taken])
+
+    return {"response": generation}
 
 if __name__ == "__main__":
     import uvicorn
