d379987b22c212246b43d0fefb3d827929bb06f5
GouravMidya <gouravgmidya@gmail.com>
2024-06-10 11:08:44 +0530

    gen response optimized

--- dev/knowledge_creation/gen_response.py
+++ dev/knowledge_creation/gen_response.py
@@ -4,6 +4,7 @@ from dev.utils.file_mover import move_files
 from dotenv import load_dotenv
 import pandas as pd
 import requests
+import shutil
 
 load_dotenv()
 folder_path = os.getenv("folder_path")
@@ -11,6 +12,7 @@ loading_folder_path = folder_path+"/data/active/csvs/questions/"
 
 for filename in os.listdir(loading_folder_path):
     if filename.endswith('.csv'):
+        print(f"Processing {filename}...")
         csv_path = os.path.join(loading_folder_path, filename)
         df = pd.read_csv(csv_path)
         questions = df['Question']
@@ -20,8 +22,10 @@ for filename in os.listdir(loading_folder_path):
             headers = {'Content-Type': 'application/json'}  # Set the appropriate content type
             response = requests.post(url, json=payload, headers=headers)
             print(response)
+        shutil.move(folder_path+"/data/active/csvs/questions/"+filename, folder_path+"/data/archieve/csvs/questions/"+filename)
+        print(f"{filename} processed succesffuly and moved to archieves")
             
-move_files(folder_path+"/data/active/csvs/questions/",folder_path+"/data/archieve/csvs/questions/","csv")
+            
 
                 
             
